91c12f57c2331b78c4edd3a0a3d7cc97aaf42d95
PieceOfEon <danilskripka1996@gmail.com>
2022-11-03 21:05:57 +0200

    end

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -36,6 +36,7 @@
             this.label1 = new System.Windows.Forms.Label();
             this.comboBox1 = new System.Windows.Forms.ComboBox();
             this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // textBox1
@@ -116,11 +117,22 @@
             this.label2.TabIndex = 7;
             this.label2.Text = "Profession";
             // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(285, 330);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "label3";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.comboBox1);
             this.Controls.Add(this.label1);
@@ -146,6 +158,7 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.ComboBox comboBox1;
         private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -12,16 +12,15 @@ namespace RegistrWindow
 {
     public partial class Form1 : Form
     {
-
-        public int kol = 0;
         public int kol2 = 0;
+        public string prf;
         struct baza
         {
             public string login;
             public string pas;
             public string proff;
         }
-        baza[] b = new baza[100];
+        baza[] b = new baza[0];
         public Form1()
         {
             InitializeComponent();
@@ -36,8 +35,7 @@ namespace RegistrWindow
         private void Registr_Click(object sender, EventArgs e)
         {
             kol2 = 0;
-            b[kol].login = textBox1.Text;
-            b[kol].pas = textBox2.Text;
+            
             if(textBox1.Text=="" || textBox1.Text.Length<3)
             {
                 MessageBox.Show("Логин должен состоять минимум из 3 символов");
@@ -63,18 +61,28 @@ namespace RegistrWindow
                 textBox2.Text = "";
                 return;
             }
+           
+            Array.Resize(ref b, b.Length + 1);
+            b[b.Length-1].login = textBox1.Text;
+            b[b.Length-1].pas = textBox2.Text;
+            b[b.Length - 1].proff = prf;
+            MessageBox.Show("Your log->> " + b[b.Length - 1].login + "\nYourpass->> " + b[b.Length - 1].pas + "\n" + "Your Proffesion->>" + b[b.Length - 1].proff);
             textBox1.Text = "";
             textBox2.Text = "";
-            MessageBox.Show("Your log->> " + b[kol].login + "\nYourpass->> "+ b[kol].pas+"\n"+"Your Proffesion->>" + b[kol].proff );
-            kol++;
-            
-            
         }
 
         private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
         {
-            b[kol].proff = comboBox1.Text;
+            prf = comboBox1.Text;
             comboBox1.Text = "";
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+            foreach(var f in b)
+            {
+                MessageBox.Show("Your log->> " + f.login + "\nYourpass->> " + f.pas + "\n" + "Your Proffesion->>" + f.proff);
+            }
         }
     }
 }
