cb3495e1a7e9b1080b7c4c4cbbe0baac6c59982d
PieceOfEon <danilskripka1996@gmail.com>
2022-11-06 18:16:38 +0200

    DTPend

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -37,6 +37,22 @@
             this.comboBox1 = new System.Windows.Forms.ComboBox();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.dateTimePicker2 = new System.Windows.Forms.DateTimePicker();
+            this.label10 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label11 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // textBox1
@@ -45,6 +61,7 @@
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(247, 20);
             this.textBox1.TabIndex = 0;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
             // textBox2
             // 
@@ -58,36 +75,40 @@
             // Log
             // 
             this.Log.AutoSize = true;
-            this.Log.Location = new System.Drawing.Point(158, 96);
+            this.Log.Font = new System.Drawing.Font("Impact", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Log.Location = new System.Drawing.Point(172, 93);
             this.Log.Name = "Log";
-            this.Log.Size = new System.Drawing.Size(25, 13);
+            this.Log.Size = new System.Drawing.Size(31, 20);
             this.Log.TabIndex = 2;
             this.Log.Text = "Log";
             // 
             // Pass
             // 
             this.Pass.AutoSize = true;
-            this.Pass.Location = new System.Drawing.Point(158, 157);
+            this.Pass.Font = new System.Drawing.Font("Impact", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Pass.Location = new System.Drawing.Point(162, 154);
             this.Pass.Name = "Pass";
-            this.Pass.Size = new System.Drawing.Size(30, 13);
+            this.Pass.Size = new System.Drawing.Size(41, 20);
             this.Pass.TabIndex = 3;
             this.Pass.Text = "Pass";
             // 
             // Registr
             // 
             this.Registr.AutoSize = true;
+            this.Registr.Font = new System.Drawing.Font("Impact", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.Registr.Location = new System.Drawing.Point(307, 293);
             this.Registr.Name = "Registr";
-            this.Registr.Size = new System.Drawing.Size(40, 13);
+            this.Registr.Size = new System.Drawing.Size(57, 20);
             this.Registr.TabIndex = 4;
             this.Registr.Text = "Registr";
             this.Registr.Click += new System.EventHandler(this.Registr_Click);
             // 
             // label1
             // 
-            this.label1.Location = new System.Drawing.Point(223, 187);
+            this.label1.Font = new System.Drawing.Font("Impact", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(216, 187);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(240, 60);
+            this.label1.Size = new System.Drawing.Size(278, 106);
             this.label1.TabIndex = 5;
             this.label1.Text = "Пароль должен быть минимум из 9 символов и содрежать один из следующих знаков \r\n[" +
     "!],[_],[+],[@],[$]\r\n";
@@ -111,27 +132,221 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(145, 38);
+            this.label2.Font = new System.Drawing.Font("Impact", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(132, 36);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(56, 13);
+            this.label2.Size = new System.Drawing.Size(80, 20);
             this.label2.TabIndex = 7;
             this.label2.Text = "Profession";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(285, 330);
+            this.label3.Font = new System.Drawing.Font("Impact", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(567, 187);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.Size = new System.Drawing.Size(43, 20);
             this.label3.TabIndex = 8;
-            this.label3.Text = "label3";
+            this.label3.Text = "Enter";
             this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(216, 93);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(247, 20);
+            this.textBox3.TabIndex = 9;
+            this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(216, 154);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(247, 20);
+            this.textBox4.TabIndex = 10;
+            this.textBox4.UseSystemPasswordChar = true;
+            this.textBox4.TextChanged += new System.EventHandler(this.textBox4_TextChanged);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Impact", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(144, 90);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(59, 23);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Логин";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Impact", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label5.Location = new System.Drawing.Point(138, 149);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(72, 23);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "Пароль";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Impact", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label6.Location = new System.Drawing.Point(252, 59);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(150, 23);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "Личный кабинет";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Impact", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label7.Location = new System.Drawing.Point(306, 224);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(50, 23);
+            this.label7.TabIndex = 14;
+            this.label7.Text = "Вход";
+            this.label7.Click += new System.EventHandler(this.label7_Click);
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "Киев",
+            "Днепр",
+            "Египет",
+            "Турция",
+            "Франция",
+            "Эмираты",
+            "Америка",
+            "Германия",
+            "Италия",
+            "Португалия"});
+            this.comboBox2.Location = new System.Drawing.Point(18, 66);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(121, 21);
+            this.comboBox2.TabIndex = 15;
+            this.comboBox2.Visible = false;
+            this.comboBox2.SelectedIndexChanged += new System.EventHandler(this.comboBox2_SelectedIndexChanged);
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Impact", 12.25F);
+            this.label8.Location = new System.Drawing.Point(14, 38);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(112, 21);
+            this.label8.TabIndex = 16;
+            this.label8.Text = "Выберите тур";
+            this.label8.Visible = false;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Impact", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label9.Location = new System.Drawing.Point(239, 39);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(136, 20);
+            this.label9.TabIndex = 17;
+            this.label9.Text = "Дата отправления";
+            this.label9.Visible = false;
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(202, 67);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(200, 20);
+            this.dateTimePicker1.TabIndex = 18;
+            this.dateTimePicker1.Visible = false;
+            // 
+            // dateTimePicker2
+            // 
+            this.dateTimePicker2.Location = new System.Drawing.Point(442, 67);
+            this.dateTimePicker2.Name = "dateTimePicker2";
+            this.dateTimePicker2.Size = new System.Drawing.Size(200, 20);
+            this.dateTimePicker2.TabIndex = 19;
+            this.dateTimePicker2.Visible = false;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Impact", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label10.Location = new System.Drawing.Point(470, 39);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(140, 20);
+            this.label10.TabIndex = 20;
+            this.label10.Text = "Дата возвращения";
+            this.label10.Visible = false;
+            // 
+            // button1
+            // 
+            this.button1.Cursor = System.Windows.Forms.Cursors.Default;
+            this.button1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.button1.Font = new System.Drawing.Font("Segoe Script", 48F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Italic | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(0, 276);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(800, 174);
+            this.button1.TabIndex = 21;
+            this.button1.Text = "Подтвердить";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Visible = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(26, 138);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(0, 13);
+            this.label11.TabIndex = 22;
+            this.label11.Visible = false;
+            this.label11.Click += new System.EventHandler(this.label11_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Impact", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button2.Location = new System.Drawing.Point(736, 28);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(52, 31);
+            this.button2.TabIndex = 23;
+            this.button2.Text = "Back";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Visible = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Impact", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button3.Location = new System.Drawing.Point(736, 90);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(52, 31);
+            this.button3.TabIndex = 24;
+            this.button3.Text = "Back";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Visible = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.dateTimePicker2);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.comboBox1);
@@ -159,6 +374,22 @@
         private System.Windows.Forms.ComboBox comboBox1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.DateTimePicker dateTimePicker2;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -12,18 +13,29 @@ namespace RegistrWindow
 {
     public partial class Form1 : Form
     {
+        
         public int kol2 = 0;
         public string prf;
-        struct baza
+        public int index;
+        public struct baza
         {
             public string login;
             public string pas;
             public string proff;
+            public string tour;
+            public string date1;
+            public string date2;
         }
         baza[] b = new baza[0];
         public Form1()
         {
             InitializeComponent();
+            label4.Visible = false;
+            label5.Visible = false;
+            label6.Visible = false;
+            label7.Visible = false;
+            textBox3.Visible = false;
+            textBox4.Visible = false;
         }
 
         private void textBox2_Enter(object sender, EventArgs e)
@@ -42,9 +54,9 @@ namespace RegistrWindow
                 textBox1.Text = "";
                 return;
             }
-            if(textBox2.Text==""||textBox2.Text.Length<9)
+            if(textBox2.Text==""||textBox2.Text.Length<32)
             {
-                MessageBox.Show("Пароль должен состоять минимум из 9 символов");
+                MessageBox.Show("Пароль должен состоять минимум из 32 символов");
                 textBox2.Text = "";
                 return;
             }
@@ -79,10 +91,170 @@ namespace RegistrWindow
 
         private void label3_Click(object sender, EventArgs e)
         {
-            foreach(var f in b)
+            
+            comboBox1.Visible = false;
+            label1.Visible = false;
+            textBox2.Visible = false;
+            textBox1.Visible = false;
+            label2.Visible = false;
+            label3.Visible = false;
+            Log.Visible = false;
+            Pass.Visible = false;
+            Registr.Visible = false;
+
+            label4.Visible = true;
+            label5.Visible = true;
+            label6.Visible = true;
+            label7.Visible = true;
+            textBox3.Visible = true;
+            textBox4.Visible = true;
+            button3.Visible = true;
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label7_Click(object sender, EventArgs e)
+        {
+           
+            for (int i = 0; i < b.Length; i++)
             {
-                MessageBox.Show("Your log->> " + f.login + "\nYourpass->> " + f.pas + "\n" + "Your Proffesion->>" + f.proff);
+                if (textBox3.Text == b[i].login)
+                {
+                    if (textBox4.Text == b[i].pas)
+                    {
+                        index = i;
+                        MessageBox.Show("Авторизация успешна");
+                        label4.Visible = false;
+                        label5.Visible = false;
+                        label6.Visible = false;
+                        label7.Visible = false;
+                        textBox3.Visible = false;
+                        textBox4.Visible = false;
+                        button3.Visible = false;
+
+                        label8.Visible = true;
+                        comboBox2.Visible = true;
+                        label9.Visible = true;
+                        dateTimePicker1.Visible = true;
+                        dateTimePicker2.Visible = true;
+                        label10.Visible = true;
+                        button1.Visible = true;
+                        button2.Visible = true;
+                        label11.Visible = true;
+                        
+                        
+                        label11.Text = "";
+                        break;
+                    }
+                }
+                if(i == b.Length-1)
+                {
+                    MessageBox.Show("Ошибка при вводе логина или пароля или его не существует");
+                }
             }
+            
+        }
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(comboBox2.Text=="")
+            {
+                MessageBox.Show("Выберите тур");
+                return;
+            }
+            if(dateTimePicker1.Value.Date>dateTimePicker2.Value.Date)
+            {
+                MessageBox.Show("Дата отправления не может быть больше даты прибытия");
+                return;
+            }
+            if (dateTimePicker1.Value.Date < DateTime.Today)
+            {
+                MessageBox.Show("Дата отправления не может быть меньше текущей");
+                return;
+            }
+            if(dateTimePicker2.Value.Date<DateTime.Today)
+            {
+                MessageBox.Show("Дата прибытия не может быть меньше текущей");
+                return;
+            }
+            b[index].tour = comboBox2.Text;
+            b[index].date1=dateTimePicker1.Text;
+            b[index].date2 = dateTimePicker2.Text;
+            MessageBox.Show("Тур- >> " + b[index].tour + "Date1 - > " + b[index].date1 + " Date2 - > " + b[index].date2);
+            //label11.Text = "";
+            label11.Text += "Тур- >> " + b[index].tour + "Дата отправления - > " + b[index].date1 + " Дата возвращения - > " + b[index].date2+"\n";
+            
+        }
+
+        private void label11_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            label4.Visible = true;
+            label5.Visible = true;
+            label6.Visible = true;
+            label7.Visible = true;
+            textBox3.Visible = true;
+            textBox4.Visible = true;
+            button3.Visible = true;
+
+            label8.Visible = false;
+            comboBox2.Visible = false;
+            label9.Visible = false;
+            dateTimePicker1.Visible = false;
+            dateTimePicker2.Visible = false;
+            label10.Visible = false;
+            button1.Visible = false;
+            button2.Visible = false;
+            label11.Visible = false;
+            button2.Visible = false;
+           
+            textBox4.Text = "";
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            button3.Visible = false;
+
+
+            comboBox1.Visible = true;
+            label1.Visible = true;
+            textBox2.Visible = true;
+            textBox1.Visible = true;
+            label2.Visible = true;
+            label3.Visible = true;
+            Log.Visible = true;
+            Pass.Visible = true;
+            Registr.Visible = true;
+
+            label4.Visible = false;
+            label5.Visible = false;
+            label6.Visible = false;
+            label7.Visible = false;
+            textBox3.Visible = false;
+            textBox4.Visible = false;
+            button3.Visible = false;
         }
     }
 }
